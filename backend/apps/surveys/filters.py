a0b2dbdc9b26b965363fd87a77644d4baf7a7ab5
heva98 <evahosea98@gmail.com>
2026-09-23 12:35:23 +0300

    remove unresoved markers

--- backend/apps/surveys/filters.py
+++ backend/apps/surveys/filters.py
@@ -1,10 +1,6 @@
 import django_filters
 
-<<<<<<< HEAD
-from .models import Beacon, RoadReserve, Subdivision, SubdivisionPlot, SurveyDocument, Surveyor, UtilityReserve
-=======
 from .models import Beacon, RoadReserve, Subdivision, SubdivisionPlot, Survey, SurveyDocument, Surveyor, UtilityReserve
->>>>>>> 4dfa5005ce0fc8cd0dd6dc643456c2e7c47e09e6
 
 
 class SurveyorFilter(django_filters.FilterSet):
@@ -13,12 +9,6 @@ class SurveyorFilter(django_filters.FilterSet):
         fields = ['company', 'is_active']
 
 
-<<<<<<< HEAD
-class BeaconFilter(django_filters.FilterSet):
-    class Meta:
-        model = Beacon
-        fields = ['survey', 'beacon_type', 'condition']
-=======
 class SurveyFilter(django_filters.FilterSet):
     class Meta:
         model = Survey
@@ -28,14 +18,13 @@ class SurveyFilter(django_filters.FilterSet):
 class BeaconFilter(django_filters.FilterSet):
     class Meta:
         model = Beacon
-        fields = ['survey', 'condition']
+        fields = ['survey', 'beacon_type', 'condition']
 
 
 class SurveyDocumentFilter(django_filters.FilterSet):
     class Meta:
         model = SurveyDocument
         fields = ['survey', 'document_type']
->>>>>>> 4dfa5005ce0fc8cd0dd6dc643456c2e7c47e09e6
 
 
 class SubdivisionFilter(django_filters.FilterSet):
@@ -60,12 +49,3 @@ class UtilityReserveFilter(django_filters.FilterSet):
     class Meta:
         model = UtilityReserve
         fields = ['subdivision', 'utility_type']
-<<<<<<< HEAD
-
-
-class SurveyDocumentFilter(django_filters.FilterSet):
-    class Meta:
-        model = SurveyDocument
-        fields = ['survey', 'document_type']
-=======
->>>>>>> 4dfa5005ce0fc8cd0dd6dc643456c2e7c47e09e6
